fe7329b19a01864d79651e8b2db863a24de6c1d9
khushalsonawat <khushalsonawat@gmail.com>
2026-07-16 17:55:37 +0530

    Fixing sidebar also

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -324,9 +324,20 @@ export const tabNavigation: NavTab[] = [
             title: 'Features',
             items: [
               { title: 'Running Evaluations', href: '/docs/evaluation/guides/running-evaluations' },
+              {
+                title: 'Explore playground',
+                items: [
+                  { title: 'Overview', href: '/docs/evaluation/guides/explore-playground' },
+                  { title: 'Test an eval', href: '/docs/evaluation/guides/explore-playground/test-an-eval' },
+                  { title: 'Usage & analytics', href: '/docs/evaluation/guides/explore-playground/usage-analytics' },
+                  { title: 'Eval logs', href: '/docs/evaluation/guides/explore-playground/eval-logs' },
+                ]
+              },
               { title: 'Create a custom eval', href: '/docs/evaluation/guides/custom-evals' },
               { title: 'Build a composite evals', href: '/docs/evaluation/guides/composite-evals' },
+              { title: 'Set up guardrails', href: '/docs/evaluation/guides/guardrails' },
               { title: 'Add ground truth', href: '/docs/evaluation/guides/ground-truth' },
+              { title: 'Collect feedback', href: '/docs/evaluation/guides/feedback' },
               { title: 'Use custom models', href: '/docs/evaluation/guides/custom-models' },
               { title: 'Evaluate in CI/CD', href: '/docs/evaluation/guides/cicd' },
               { title: 'Advanced usage', href: '/docs/evaluation/guides/advanced-usage' },
